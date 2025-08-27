92d3057c8c9a04dec4166886e05debccadbb0c93
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2025-08-27 16:56:28 +0700

    feat: Implement ProductController and update routes for product management

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -15,5 +15,54 @@
 @endsection
 
 @section('content')
-    <p>Welcome to the products page!</p>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th style="width: 10px">#</th>
+                                <th>Product Name</th>
+                                <th>Description</th>
+                                <th>Product Code</th>
+                                <th>Category</th>
+                                <th>Price</th>
+                                <th style="width: 40px">Stock</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1.</td>
+                                <td>Product 1</td>
+                                <td>This is product 1</td>
+                                <td>PROD001</td>
+                                <td>Category A</td>
+                                <td>$10.00</td>
+                                <td>100</td>
+                            </tr>
+                            <tr>
+                                <td>2.</td>
+                                <td>Product 2</td>
+                                <td>This is product 2</td>
+                                <td>PROD002</td>
+                                <td>Category B</td>
+                                <td>$20.00</td>
+                                <td>50</td>
+                            </tr>
+                            <tr>
+                                <td>3.</td>
+                                <td>Product 3</td>
+                                <td>This is product 3</td>
+                                <td>PROD003</td>
+                                <td>Category A</td>
+                                <td>$15.00</td>
+                                <td>75</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages/products/index');
-});
+
+Route::get('/', [ProductController::class, 'index'])->name('dashboard');
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
