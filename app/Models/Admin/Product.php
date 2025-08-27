2c97ab019a56f2fc762bd08eb1683f5557a55675
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2025-08-27 16:53:19 +0700

    feat: Create Category and Product models with migrations for inventory management

---
+++ app/Models/Admin/Product.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $guarded = [];
+    protected $fillable = ['name', 'description', 'stock_keeping_unit', 'price', 'quantity_in_stock', 'category_id'];
+}
