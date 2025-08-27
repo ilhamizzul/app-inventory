d2ebe7e279eb13c6736dd576b86acdd694cf7d67
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2025-08-27 15:19:55 +0700

    feat: Add main layout, navbar, and sidebar for inventory management system
    - Created main layout file with header, footer, and content sections.
    - Implemented a responsive navbar with dropdowns for messages and notifications.
    - Added a sidebar for navigation with a dashboard link.
    - Developed a products index page with breadcrumb navigation.
    - Updated the web routes to point to the products index page.

---
+++ resources/views/layouts/components/sidebar.blade.php
@@ -0,0 +1,24 @@
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+    <div class="sidebar-brand">
+        <a href="./index.html" class="brand-link">
+            <img src="{{ asset('Templates/dist/assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                class="brand-image opacity-75 shadow" />
+            <span class="brand-text fw-light">Inventory Management</span>
+        </a>
+    </div>
+    <div class="sidebar-wrapper">
+        <nav class="mt-2">
+            <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="navigation"
+                aria-label="Main navigation" data-accordion="false" id="navigation">
+                <li class="nav-item">
+                    <a href="#" class="nav-link active">
+                        <i class="nav-icon bi bi-speedometer"></i>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
---
+++ resources/views/pages/products/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+
+@section('header')
+    <div class="row">
+        <div class="col-sm-6">
+            <h3 class="mb-0">Products</h3>
+        </div>
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-end">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Products</li>
+            </ol>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+    <p>Welcome to the products page!</p>
+@endsection
